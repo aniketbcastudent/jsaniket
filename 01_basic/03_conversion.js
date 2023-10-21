48a812b7a5c14776fe3d26e1c816380979a29077
aniketbcastudent <148610749+aniketbcastudent@users.noreply.github.com>
2023-10-21 15:33:34 +0000

    comparision

--- 01_basic/03_conversion.js
+++ 01_basic/03_conversion.js
@@ -1,18 +1,57 @@
-// //let scroe=33;
-// let scroe="33avd";
-// console.log(typeof scroe);
-// console.log(typeof scroe);
-// //let scroe="33avd";
-// let valveInNumber= Number(scroe);
-// console.log(typeof valveInNumber);
-// console.log(valveInNumber);
+// // //let scroe=33;
+// // let scroe="33avd";
+// // console.log(typeof scroe);
+// // console.log(typeof scroe);
+// // //let scroe="33avd";
+// // let valveInNumber= Number(scroe);
+// // console.log(typeof valveInNumber);
+// // console.log(valveInNumber);
 
-let isoggedIN =33
-let booleanIsLoggin=Boolean(isoggedIN)
-console.log(booleanIsLoggin);
-//1=> true; 0=> false
-//""=>false
-//"aniket"=>true
-let somenumber =33
-let stringNumber =String(somenumber)
-console.log(typeof stringNumber);
+// let isoggedIN =33
+// let booleanIsLoggin=Boolean(isoggedIN)
+// console.log(booleanIsLoggin);
+// //1=> true; 0=> false
+// //""=>false
+// //"aniket"=>true
+// let somenumber =33
+// let stringNumber =String(somenumber)
+// console.log(typeof stringNumber);
+//*****************************************operations********* */
+// let value =3
+// let negValue=-value
+// console.log(negValue);
+
+// let str1="hello"
+// let srt2="aniket"
+// console.log(str1 + srt2);
+
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log(1+"2"+2);
+// console.log(1+2+"2");//output 32
+// console.log((3+4)*5%3);
+// console.log(+true);//output 1
+// console.log(+"");//output 0
+
+
+// let gameCounter=100
+// //++gameCounter;prefix operater
+// gameCounter++//opstfix operater
+// console.log(gameCounter);
+
+
+
+// console.log("2">1);//output is true
+// console.log("02">1);//output is true
+// console.log(null >0);//output is false
+// console.log(null ==0);//output is false
+// console.log(null >=0);//output is true
+
+
+
+console.log("2"===2); // output is false because not convert into "numbeer" so output is number
+
+
+
+
