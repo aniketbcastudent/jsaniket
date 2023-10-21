3934adacf9552952774e978a4c74ad5cfa9a4d63
aniketbcastudent <148610749+aniketbcastudent@users.noreply.github.com>
2023-10-21 16:00:00 +0000

    datatype summary

---
+++ 01_basic/datatype-summary.js
@@ -0,0 +1,25 @@
+//primitive
+
+//7 :String, Number, Boolearn,null, undefined,Symbol,BigInt
+// const id =Symbol('123');
+// const anothertId= Symbol("123")
+// console.log(id===anothertId);//output is false Symbol is spacal data type 
+ const bigNumber =2345675657357n//bigint data  type
+
+
+
+//Reference( non primitive) 
+//Array ,objects, functions
+ const hero=["aniket kumar","aman","amar"] //this is  an array
+
+
+  let myobj ={//this is object{object ka return type hai object  }
+    name:"aniket",
+    age:34,
+ }
+
+ const myfunction=  function(){//function ka return type hai function
+     console.log("hello world");
+ }
+ console.log(typeof myobj);
+
