9493d83acf2384478a2a9d4530b4fde10c2ae0e0
aniketbcastudent <148610749+aniketbcastudent@users.noreply.github.com>
2023-10-23 00:54:43 +0000

    array

---
+++ 02_basic/array.js
@@ -0,0 +1,21 @@
+//array
+// const  myarray=[1,2,3,4,5,6,true,"aniket"]
+// const  myarray=[1,2,3,4,5,6,]
+// const myarr=new Array(1,2,34,4,56,)
+// myarr.push(6)
+// myarr.push(7)//add element from lest index
+// myarr.pop()//remove the element from last index
+// myarr.unshift(9)//shift first index
+// myarr.shift()//remove the element from first index
+// console.log(myarr.includes(2));//find the element from array and return boolean value true and false
+// console.log(myarr.indexOf(34));//if element is present return index value else return -1 that mean false
+
+
+
+// const newarr=[]
+// console.log(myarr);
+
+// console.log(myarray[0]);
+
+
+
