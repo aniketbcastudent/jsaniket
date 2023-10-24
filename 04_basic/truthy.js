a72a310c28d1efacdd66421a72660acc5b22401d
aniketbcastudent <148610749+aniketbcastudent@users.noreply.github.com>
2023-10-24 11:55:07 +0000

    truthy

---
+++ 04_basic/truthy.js
@@ -0,0 +1,23 @@
+// falsy values
+//false, 0,-0,bigint,0n,null,undefined,NaN
+//truthy values
+//"0",'false'," ,[],{},function(){} 
+
+const useremail=[]//if array is empty
+if (useremail.length===0) {
+    console.log("Array is empty"); 
+}
+
+const emptyobject={}
+if (Object.keys(emptyobject).length===0) {
+    console.log("object is empty");
+}
+//nullish Coalescing operator(??):null undefined
+let val1
+// val1 =5?? 10
+// val1 =null??10
+// val1 =undefined??15
+// val1=null ?? 10??20
+// console.log(val1);
+//(****************//Terniary operator************)
+//condition ? true: false
