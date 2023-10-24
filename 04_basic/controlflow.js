60d2976106e593942a67fb009e85ae3be6424679
aniketbcastudent <148610749+aniketbcastudent@users.noreply.github.com>
2023-10-24 11:54:47 +0000

    flow

---
+++ 04_basic/controlflow.js
@@ -0,0 +1,33 @@
+//if
+const months=3;
+switch (months) {
+  
+    case 1:
+        console.log("jan");
+        
+        break;
+    case 2:
+        console.log("fab");
+        
+        break;
+    case 3:
+        console.log("march");
+        
+        break;
+    case 4:
+        console.log("april");
+        
+        break;
+    case 5:
+        console.log("may");
+        
+        break;
+    case 6:
+        console.log("june");
+        
+        break;
+
+    default:
+        console.log("defalut");
+        break;
+}
